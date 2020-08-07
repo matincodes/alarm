f8961fe100ca78253f20df4fc989763d80862a1e
temilolasayo <alajikitemilola@gmail.com>
2020-08-07 13:41:03 +0100

    Implemented the current date and time in the clock text section

--- index.js
+++ index.js
@@ -23,3 +23,40 @@ btnPlus.addEventListener('click', function(){
   var time = document.getElementById("time");
   time.classList.toggle("active");
 })
+
+function showTime()
+{
+	var date = new Date();
+	var h = date.getHours(); // 0 - 23
+	var m = date.getMinutes(); // 0 - 59
+	var s = date.getSeconds(); // 0 - 59
+	var session = "AM";
+ 
+	if(h == 0){
+		h = 12;
+	}
+ 
+	if(h > 12){
+		h = h - 12;
+		session = "PM";
+		}
+ 
+		h = (h < 10) ? "0" + h : h;
+		m = (m < 10) ? "0" + m : m;
+		s = (s < 10) ? "0" + s : s;
+ 
+		var time = h + ":" + m + ":" + s + " " + session;
+		document.getElementById("clock-text__time").innerText = time;
+		document.getElementById("clock-text__time").textContent = time;
+
+		var today = new Date();
+
+		var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+		var date = today.toLocaleDateString("en-US", options);
+ 		document.getElementById("clock-text__date").innerText = date;
+ 		document.getElementById("clock-text__date").textContent = date;
+
+		setTimeout(showTime, 1000);
+}
+ 
+			showTime();
